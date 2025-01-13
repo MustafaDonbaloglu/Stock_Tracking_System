0f953ed0f405856db08e456e5f849400108271bf
musta <musta@ABRA>
2025-01-13 06:48:12 +0300

    Proje dosyası ekle.

---
+++ Stock_Tracking_System/Form1.cs
@@ -0,0 +1,50 @@
+namespace Stock_Tracking_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void envanterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ürün2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ürün4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
