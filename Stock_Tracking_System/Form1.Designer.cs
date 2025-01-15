e1ced1c479e84d07854c5bec01c135c56026c8c2
MUSTAFA DONBALOGLU <220507059@kafkas.edu.tr>
2025-01-15 10:36:49 +0300

    success

--- Stock_Tracking_System/Form1.Designer.cs
+++ Stock_Tracking_System/Form1.Designer.cs
@@ -1,16 +1,14 @@
-﻿namespace Stock_Tracking_System
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stock_Tracking_System
 {
-    partial class Form1
+    partial class Form1 : Form  // Form sınıfından kalıtım aldığından emin olun
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,227 +18,139 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            menuStrip1 = new MenuStrip();
-            envanterToolStripMenuItem = new ToolStripMenuItem();
-            ürün2ToolStripMenuItem = new ToolStripMenuItem();
-            ürün3ToolStripMenuItem = new ToolStripMenuItem();
-            ürün4ToolStripMenuItem = new ToolStripMenuItem();
-            ürün5ToolStripMenuItem = new ToolStripMenuItem();
-            ürün6ToolStripMenuItem = new ToolStripMenuItem();
-            ürün6ToolStripMenuItem1 = new ToolStripMenuItem();
-            ürün1ToolStripMenuItem = new ToolStripMenuItem();
-            ürün2ToolStripMenuItem1 = new ToolStripMenuItem();
-            ürün3ToolStripMenuItem1 = new ToolStripMenuItem();
-            ürün4ToolStripMenuItem1 = new ToolStripMenuItem();
-            contextMenuStrip1 = new ContextMenuStrip(components);
-            contextMenuStrip2 = new ContextMenuStrip(components);
-            menüToolStripMenuItem = new ToolStripMenuItem();
-            listView1 = new ListView();
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
             label1 = new Label();
-            textBox1 = new TextBox();
             label2 = new Label();
-            button1 = new Button();
-            menuStrip1.SuspendLayout();
-            contextMenuStrip2.SuspendLayout();
+            label3 = new Label();
+            label4 = new Label();
+            txtUrunID = new TextBox();
+            txtUrunAdi = new TextBox();
+            txtMiktar = new TextBox();
+            txtFiyat = new TextBox();
+            btnEkle = new Button();
+            btnGuncelle = new Button();
+            btnSil = new Button();
+            ((ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
-            // menuStrip1
+            // dataGridView1
             // 
-            menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { envanterToolStripMenuItem, ürün2ToolStripMenuItem, ürün3ToolStripMenuItem, ürün4ToolStripMenuItem, ürün5ToolStripMenuItem, ürün6ToolStripMenuItem, ürün6ToolStripMenuItem1 });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 28);
-            menuStrip1.TabIndex = 0;
-            menuStrip1.Text = "menuStrip1";
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            resources.ApplyResources(dataGridView1, "dataGridView1");
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.CellClick += dataGridView1_CellClick;
             // 
-            // envanterToolStripMenuItem
+            // groupBox1
             // 
-            envanterToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ürün1ToolStripMenuItem, ürün2ToolStripMenuItem1, ürün3ToolStripMenuItem1, ürün4ToolStripMenuItem1 });
-            envanterToolStripMenuItem.Name = "envanterToolStripMenuItem";
-            envanterToolStripMenuItem.Size = new Size(80, 24);
-            envanterToolStripMenuItem.Text = "Katgori1";
-            envanterToolStripMenuItem.Click += envanterToolStripMenuItem_Click;
-            // 
-            // ürün2ToolStripMenuItem
-            // 
-            ürün2ToolStripMenuItem.Name = "ürün2ToolStripMenuItem";
-            ürün2ToolStripMenuItem.Size = new Size(88, 24);
-            ürün2ToolStripMenuItem.Text = "Katagori2";
-            ürün2ToolStripMenuItem.Click += ürün2ToolStripMenuItem_Click;
-            // 
-            // ürün3ToolStripMenuItem
-            // 
-            ürün3ToolStripMenuItem.Name = "ürün3ToolStripMenuItem";
-            ürün3ToolStripMenuItem.Size = new Size(62, 24);
-            ürün3ToolStripMenuItem.Text = "Ürün3";
-            // 
-            // ürün4ToolStripMenuItem
-            // 
-            ürün4ToolStripMenuItem.Name = "ürün4ToolStripMenuItem";
-            ürün4ToolStripMenuItem.Size = new Size(88, 24);
-            ürün4ToolStripMenuItem.Text = "Katagori4";
-            ürün4ToolStripMenuItem.Click += ürün4ToolStripMenuItem_Click;
-            // 
-            // ürün5ToolStripMenuItem
-            // 
-            ürün5ToolStripMenuItem.Name = "ürün5ToolStripMenuItem";
-            ürün5ToolStripMenuItem.Size = new Size(62, 24);
-            ürün5ToolStripMenuItem.Text = "Ürün5";
-            // 
-            // ürün6ToolStripMenuItem
-            // 
-            ürün6ToolStripMenuItem.Name = "ürün6ToolStripMenuItem";
-            ürün6ToolStripMenuItem.Size = new Size(88, 24);
-            ürün6ToolStripMenuItem.Text = "Katagori6";
-            // 
-            // ürün6ToolStripMenuItem1
-            // 
-            ürün6ToolStripMenuItem1.Name = "ürün6ToolStripMenuItem1";
-            ürün6ToolStripMenuItem1.Size = new Size(97, 24);
-            ürün6ToolStripMenuItem1.Text = "Kataogroi7";
-            // 
-            // ürün1ToolStripMenuItem
-            // 
-            ürün1ToolStripMenuItem.Name = "ürün1ToolStripMenuItem";
-            ürün1ToolStripMenuItem.Size = new Size(224, 26);
-            ürün1ToolStripMenuItem.Text = "ürün1";
-            // 
-            // ürün2ToolStripMenuItem1
-            // 
-            ürün2ToolStripMenuItem1.Name = "ürün2ToolStripMenuItem1";
-            ürün2ToolStripMenuItem1.Size = new Size(224, 26);
-            ürün2ToolStripMenuItem1.Text = "ürün2";
-            // 
-            // ürün3ToolStripMenuItem1
-            // 
-            ürün3ToolStripMenuItem1.Name = "ürün3ToolStripMenuItem1";
-            ürün3ToolStripMenuItem1.Size = new Size(224, 26);
-            ürün3ToolStripMenuItem1.Text = "ürün3";
-            // 
-            // ürün4ToolStripMenuItem1
-            // 
-            ürün4ToolStripMenuItem1.Name = "ürün4ToolStripMenuItem1";
-            ürün4ToolStripMenuItem1.Size = new Size(224, 26);
-            ürün4ToolStripMenuItem1.Text = "ürün4";
-            // 
-            // contextMenuStrip1
-            // 
-            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
-            contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(61, 4);
-            // 
-            // contextMenuStrip2
-            // 
-            contextMenuStrip2.ImageScalingSize = new Size(20, 20);
-            contextMenuStrip2.Items.AddRange(new ToolStripItem[] { menüToolStripMenuItem });
-            contextMenuStrip2.Name = "contextMenuStrip2";
-            contextMenuStrip2.Size = new Size(116, 28);
-            // 
-            // menüToolStripMenuItem
-            // 
-            menüToolStripMenuItem.Name = "menüToolStripMenuItem";
-            menüToolStripMenuItem.Size = new Size(115, 24);
-            menüToolStripMenuItem.Text = "menü";
-            // 
-            // listView1
-            // 
-            listView1.Location = new Point(27, 89);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(740, 168);
-            listView1.TabIndex = 3;
-            listView1.UseCompatibleStateImageBehavior = false;
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(txtUrunID);
+            groupBox1.Controls.Add(txtUrunAdi);
+            groupBox1.Controls.Add(txtMiktar);
+            groupBox1.Controls.Add(txtFiyat);
+            groupBox1.Controls.Add(btnEkle);
+            groupBox1.Controls.Add(btnGuncelle);
+            groupBox1.Controls.Add(btnSil);
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(0, 42);
+            resources.ApplyResources(label1, "label1");
             label1.Name = "label1";
-            label1.Size = new Size(84, 20);
-            label1.TabIndex = 4;
-            label1.Text = "Ürün Adedi";
-            label1.Click += label1_Click;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(275, 338);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(239, 27);
-            textBox1.TabIndex = 5;
-            textBox1.Text = "Stok gir";
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(331, 300);
+            resources.ApplyResources(label2, "label2");
             label2.Name = "label2";
-            label2.Size = new Size(84, 20);
-            label2.TabIndex = 6;
-            label2.Text = "Stok Giriniz";
-            label2.Click += label2_Click;
             // 
-            // button1
+            // label3
             // 
-            button1.Location = new Point(331, 389);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 7;
-            button1.Text = "Kaydet";
-            button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            // 
+            // txtUrunID
+            // 
+            resources.ApplyResources(txtUrunID, "txtUrunID");
+            txtUrunID.Name = "txtUrunID";
+            txtUrunID.ReadOnly = true;
+            txtUrunID.TextChanged += txtUrunID_TextChanged;
+            // 
+            // txtUrunAdi
+            // 
+            resources.ApplyResources(txtUrunAdi, "txtUrunAdi");
+            txtUrunAdi.Name = "txtUrunAdi";
+            txtUrunAdi.TextChanged += txtUrunAdi_TextChanged;
+            // 
+            // txtMiktar
+            // 
+            resources.ApplyResources(txtMiktar, "txtMiktar");
+            txtMiktar.Name = "txtMiktar";
+            txtMiktar.TextChanged += txtMiktar_TextChanged;
+            // 
+            // txtFiyat
+            // 
+            resources.ApplyResources(txtFiyat, "txtFiyat");
+            txtFiyat.Name = "txtFiyat";
+            // 
+            // btnEkle
+            // 
+            resources.ApplyResources(btnEkle, "btnEkle");
+            btnEkle.Name = "btnEkle";
+            btnEkle.Click += btnEkle_Click;
+            // 
+            // btnGuncelle
+            // 
+            resources.ApplyResources(btnGuncelle, "btnGuncelle");
+            btnGuncelle.Name = "btnGuncelle";
+            btnGuncelle.Click += btnGuncelle_Click;
+            // 
+            // btnSil
+            // 
+            resources.ApplyResources(btnSil, "btnSil");
+            btnSil.Name = "btnSil";
+            btnSil.Click += btnSil_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
-            Controls.Add(listView1);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
             Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            contextMenuStrip2.ResumeLayout(false);
+            ((ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
-        #endregion
-
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem envanterToolStripMenuItem;
-        private ToolStripMenuItem ürün2ToolStripMenuItem;
-        private ToolStripMenuItem ürün3ToolStripMenuItem;
-        private ToolStripMenuItem ürün4ToolStripMenuItem;
-        private ToolStripMenuItem ürün5ToolStripMenuItem;
-        private ToolStripMenuItem ürün6ToolStripMenuItem;
-        private ToolStripMenuItem ürün6ToolStripMenuItem1;
-        private ToolStripMenuItem ürün1ToolStripMenuItem;
-        private ToolStripMenuItem ürün2ToolStripMenuItem1;
-        private ToolStripMenuItem ürün3ToolStripMenuItem1;
-        private ToolStripMenuItem ürün4ToolStripMenuItem1;
-        private ContextMenuStrip contextMenuStrip1;
-        private ContextMenuStrip contextMenuStrip2;
-        private ToolStripMenuItem menüToolStripMenuItem;
-        private ListView listView1;
-        private Label label1;
-        private TextBox textBox1;
-        private Label label2;
-        private Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUrunID;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
     }
 }
--- Stock_Tracking_System/Form1.cs
+++ Stock_Tracking_System/Form1.cs
@@ -1,30 +1,153 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 namespace Stock_Tracking_System
 {
     public partial class Form1 : Form
     {
+        // Veritabaný baðlantý dizesi
+        private readonly string connectionString = "Data Source=Abra;Initial Catalog=StokDB; Integrated Security=True; TrustServerCertificate=True";
+
         public Form1()
         {
             InitializeComponent();
+            StokListele();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void StokListele()
         {
-
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Urunler", conn))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Veritabaný baðlantý hatasý: {ex.Message}");
+            }
         }
 
-        private void envanterToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnEkle_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtUrunAdi.Text) || string.IsNullOrWhiteSpace(txtMiktar.Text) || string.IsNullOrWhiteSpace(txtFiyat.Text))
+            {
+                MessageBox.Show("Lütfen tüm alanlarý doldurun!");
+                return;
+            }
 
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO Urunler (UrunAdi, Miktar, Fiyat) VALUES (@UrunAdi, @Miktar, @Fiyat)", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@UrunAdi", txtUrunAdi.Text);
+                        cmd.Parameters.AddWithValue("@Miktar", int.Parse(txtMiktar.Text));
+                        cmd.Parameters.AddWithValue("@Fiyat", decimal.Parse(txtFiyat.Text));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Ürün baþarýyla eklendi.");
+                StokListele();
+                Temizle();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluþtu: {ex.Message}");
+            }
         }
 
-        private void ürün2ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnGuncelle_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtUrunID.Text))
+            {
+                MessageBox.Show("Lütfen güncellenecek ürünü seçin!");
+                return;
+            }
 
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("UPDATE Urunler SET UrunAdi = @UrunAdi, Miktar = @Miktar, Fiyat = @Fiyat WHERE UrunID = @UrunID", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@UrunAdi", txtUrunAdi.Text);
+                        cmd.Parameters.AddWithValue("@Miktar", int.Parse(txtMiktar.Text));
+                        cmd.Parameters.AddWithValue("@Fiyat", decimal.Parse(txtFiyat.Text));
+                        cmd.Parameters.AddWithValue("@UrunID", int.Parse(txtUrunID.Text));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Ürün baþarýyla güncellendi.");
+                StokListele();
+                Temizle();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluþtu: {ex.Message}");
+            }
         }
 
-        private void ürün4ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnSil_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtUrunID.Text))
+            {
+                MessageBox.Show("Lütfen silinecek ürünü seçin!");
+                return;
+            }
 
+            if (MessageBox.Show("Bu ürünü silmek istediðinizden emin misiniz?", "Silme Onayý", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("DELETE FROM Urunler WHERE UrunID = @UrunID", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@UrunID", int.Parse(txtUrunID.Text));
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    MessageBox.Show("Ürün baþarýyla silindi.");
+                    StokListele();
+                    Temizle();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Hata oluþtu: {ex.Message}");
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtUrunID.Text = row.Cells["UrunID"].Value?.ToString();
+                txtUrunAdi.Text = row.Cells["UrunAdi"].Value?.ToString();
+                txtMiktar.Text = row.Cells["Miktar"].Value?.ToString();
+                txtFiyat.Text = row.Cells["Fiyat"].Value?.ToString();
+            }
+        }
+
+        private void Temizle()
+        {
+            txtUrunID.Clear();
+            txtUrunAdi.Clear();
+            txtMiktar.Clear();
+            txtFiyat.Clear();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,17 +155,17 @@ namespace Stock_Tracking_System
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void txtUrunID_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void txtUrunAdi_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void txtMiktar_TextChanged(object sender, EventArgs e)
         {
 
         }
